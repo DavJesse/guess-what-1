40a4686d29c83175221afd46d06c92e221c232a5
davodhiambo <jesseomolo@yahoo.com>
2024-05-17 12:10:31 +0300

    code: Itoa addition

---
+++ stringManipulation.go
@@ -0,0 +1,64 @@
+package main
+
+func hasSuffix(str, subStr string) bool {
+	var status bool
+	diff := len(str) - len(subStr)
+
+	if str[diff:] == subStr {
+		status = true
+	}
+	return status
+}
+
+func splitString(str string, sep string) []string {
+	var result []string
+	var token string
+
+	for i := 0; i < len(str); i++ {
+		if i <= len(str)-len(sep) && str[i:i+len(sep)] == sep {
+			result = append(result, token)
+			token = ""
+		} else {
+			token += string(str[i])
+		}
+	}
+	result = append(result, token)
+
+	for i := 0; i < len(result); i++ {
+		if result[i] == "" {
+			if i < len(result)-1 {
+				result = append(result[:i], result[i+1:]...)
+			} else {
+				result = result[:i]
+			}
+		}
+	}
+
+	return result
+}
+
+func Itoa(num int) string {
+	var div int
+	var char int32
+	var mod int
+	var result string
+	var numSlc []int
+
+	if num < 0 {
+		num *= -1
+	}
+
+	div = num
+
+	for div > 0 {
+		mod = div%10
+		div /= 10
+
+		numSlc = append(numSlc, mod)
+	}
+	
+	for _, n := range numSlc {
+		char = int32(n) + '0'
+	}
+
+}
---
+++ typeManipulation.go
@@ -0,0 +1,45 @@
+package main
+
+import "os"
+
+func Atoi(str string) (int, string) {
+	var result int
+	var rtnStr string
+
+	var digit int
+	sign := 1
+
+	for i, r := range str {
+		if i == 0 && (r == '-' || r == '+') {
+			if r == '-' {
+				sign = -1
+			}
+			continue
+		}
+		if r < '0' || r > '9' {
+			rtnStr = "Your data set contains non-numeric charaters"
+		}
+		digit = int(r - '0')
+		result = result*10 + digit
+	}
+	result *= sign
+
+	return result, rtnStr
+}
+
+func sliceTransfm(strSlc []string) []int {
+	var numSlc []int
+	var num int
+	var str string
+	for _, v := range strSlc {
+		num, str = Atoi(v)
+		if str != "" {
+			printLn(str)
+			os.Exit(0)
+		} else {
+			numSlc = append(numSlc, num)
+		}
+	}
+
+	return numSlc
+}
