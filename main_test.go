07eb6e5431e075af617f04d0291efa2b14b46c53
davodhiambo <jesseomolo@yahoo.com>
2024-05-19 17:40:54 +0300

    Unit-Testing: Test Average

--- main_test.go
+++ main_test.go
@@ -2,18 +2,32 @@ package main
 
 import "testing"
 
-func testAverage(*testing) {
+func TestAverage(t *testing.T) {
+	intSlc := []int{1, 2, 3}
+	got := average(intSlc)
+	expected := float64(2)
 
+	if got != expected {
+		t.Errorf("Got: %f", got)
+		t.Errorf("Expected: %f", expected)
+		t.Errorf("TestAverage Failed!")
+	}
 }
 
-func testMedian(*testing) {
+func TestMedian(t *testing.T) {
+	intSlc := []int{1, 2, 3}
+	got := median(intSlc)
+	expected := float64(2)
 
+	if got != expected {
+		t.Errorf("Got: %f", got)
+		t.Errorf("Expected: %f", expected)
+		t.Error("TestMedian Failed")
+	}
 }
 
-func testVariance(*testing) {
+// func testVariance(*testing) {
+// }
 
-}
-
-func testStandardDeviation(*testing) {
-
-}
+// func testStandardDeviation(*testing) {
+// }
