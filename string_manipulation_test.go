4c1930a3170f8c188c18997ee25a0d14eb701372
davodhiambo <jesseomolo@yahoo.com>
2024-05-19 18:45:10 +0300

    Unit-Testing: add TestSplitString

--- string_manipulation_test.go
+++ string_manipulation_test.go
@@ -15,6 +15,21 @@ func TestHasSuffix(t *testing.T) {
 	}
 }
 
+func TestSplitstring(t *testing.T) {
+	str := "Unit-Testing"
+	sep := "-"
+	got := splitString(str, sep)
+	expected := []string{"Unit", "Testing"}
+
+	for i := 0; i < len(expected); i++ {
+		if got[i] != expected[i] || len(got) != len(expected) {
+			t.Errorf("Got: %s", got)
+			t.Errorf("Expected: %s", expected)
+			t.Errorf("Test SplitStringFailed!")
+		}
+	}
+}
+
 // got := hasSuffix(str, subStr)
 // 	expected := true
 
