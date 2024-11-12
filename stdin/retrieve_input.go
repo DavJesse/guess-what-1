916c5b4ae7956d228d34b54e8c60b4a8d59db06b
davodhiambo <jesseomolo@yahoo.com>
2024-11-12 21:42:14 +0300

    add ExtractInput to input package

--- stdin/retrieve_input.go
+++ stdin/retrieve_input.go
@@ -1,1 +1,25 @@
-package stdin
+package input
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+// Extract user input from standaord input
+func ExtractInput() string {
+	var result string
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		result := scanner.Text()
+		if result != "" {
+			return result
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return result
+}
