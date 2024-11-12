ec09cb53151454e6ef79d74b220b8f6ec008fe19
davodhiambo <jesseomolo@yahoo.com>
2024-11-13 00:18:16 +0300

    refactor: rework code to keep scanner open.

--- main.go
+++ main.go
@@ -1,35 +1,42 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"math"
+	"os"
 
-	"math-skills/cli"
 	"math-skills/format"
 	"math-skills/maths"
-	"math-skills/stdout"
 )
 
 func main() {
-	var dataSet []int
+	var dataSet []int = make([]int, 0)
 
-	input := cli.ExtractInput()
-	num, err := format.Atoi(input)
-	if err != "" {
-		log.Fatal(err)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		num, err := format.Atoi(scanner.Text())
+		if err != "" {
+			log.Fatal(err)
+		}
+		dataSet = append(dataSet, num)
+
+		// Calculate average and standard deviation
+		average := maths.Average(dataSet)
+		variance := maths.Variance(dataSet, average)
+		standardDeviation := maths.StandardDeviation(variance)
+
+		// Establish upper and lower limits
+		upperLimit := int(math.Round(average + 2*standardDeviation))
+		lowerLimit := int(math.Round(average - 2*standardDeviation))
+
+		fmt.Printf("%d --> the standard input\n", num)
+		fmt.Printf("%d %d    --> the range for the next input, in this case for the number %d\n", lowerLimit, upperLimit, num)
+
 	}
 
-	dataSet = append(dataSet, num)
-
-	// Calculate average and standard deviation
-	average := maths.Average(dataSet)
-	variance := maths.Variance(dataSet, average)
-	standardDeviation := maths.StandardDeviation(variance)
-
-	// Establish upper and lower limits
-	upperLimit := int(math.Round(average + 2*standardDeviation))
-	lowerLimit := int(math.Round(average - 2*standardDeviation))
-
-	
-	stdout.PrintLn(format.Itoa(upperLimit) + " - " + format.Itoa(lowerLimit))
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
